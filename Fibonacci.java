d5684ecd5ab9ac599acdce10fdd1b12b87d6cb90
KOTHAMALI <kpr953@gmail.com>
2016-11-02 17:42:18 -0400

    Add files via upload

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,16 +1,18 @@
 package assignment1;
 
+import java.util.Scanner;
 
 public class Fibonacci {
 	 
-	public static void Fibo(){ // used static to avoid object creation while calling this method
+	public static void Fibo(int x){ // used static to avoid object creation while calling this method
+		
 		int num1 = 1;
 		int num2 = 2;
 		int num3;
 		
 		System.out.println("the fibonacci series is:");
 		System.out.print(num1 +","+num2+",");
-		for(int i=0;i<=15;i++) // iterate till i value reaches it limit
+		for(int i=1;i<=x-2;i++) // iterate till i value reaches it limit
 		{
 			num3= num1 + num2;
 			System.out.print(num3+",");
@@ -24,8 +26,11 @@ public class Fibonacci {
 	}
 	
 	public static void main(String[] args) { // used static during method creation
-		Fibo();                             // no need of method creation
-		
+		Scanner S = new Scanner(System.in);
+		System.out.println("enter no");
+		int x = S.nextInt();
+		Fibo(x);                             // no need of method creation
+		S.close();
 	}
 	
 
